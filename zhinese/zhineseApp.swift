fd0aabcf5b5cd4188aed2887728f697273a81659
StarLight842 <842starlite@gmail.com>
2024-12-24 23:23:40 -0800

    Initial Commit

---
+++ zhinese/zhineseApp.swift
@@ -0,0 +1,17 @@
+//
+//  zhineseApp.swift
+//  zhinese
+//
+//  Created by Aadish Verma on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct zhineseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
